f04fc773bf3dce778f1870d0c1f6cafc8d6c3063
Kunal Bhadana <130590476+Kunal-5020@users.noreply.github.com>
2026-05-15 13:24:23 +0530

    Revert "test"
    This reverts commit e64e2e5955aff81298afbbd0b7be9ec5e1b29bd3.

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "src/server.ts" },
-    nitro: { 
-      preset: "vercel",
-      publicAssets: [
-        {
-          dir: "dist/client",
-          maxAge: 31536000
-        }
-      ]
-    },
+    nitro: { preset: "vercel-edge" },
   },
 });
