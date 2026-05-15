d938bfc20b53d7764049f28859d666274efc30c6
Kunal Bhadana <130590476+Kunal-5020@users.noreply.github.com>
2026-05-15 13:36:24 +0530

    Update index.js

--- api.bak/index.js
+++ api.bak/index.js
@@ -1,3 +1,3 @@
-import { fetch } from '../dist/server/server.js';
-export const config = { runtime: 'edge' };
+import { fetch } from "../dist/server/server.js";
+export const config = { runtime: "edge" };
 export default fetch;
