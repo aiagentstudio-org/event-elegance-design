aa4bbd6144c33d003fd2a55ed6d0b0486169ecfd
Kunal Bhadana <130590476+Kunal-5020@users.noreply.github.com>
2026-05-15 12:07:02 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import handler from "@tanstack/react-start/server-entry";
 
 export const fetch = (request: Request, env: unknown, ctx: unknown) => {
-  return handler.fetch(request, { 
-    context: { env, ctx } 
+  return handler.fetch(request, {
+    context: { env, ctx },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } as any);
 };
 
