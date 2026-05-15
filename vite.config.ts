e64e2e5955aff81298afbbd0b7be9ec5e1b29bd3
Kunal Bhadana <130590476+Kunal-5020@users.noreply.github.com>
2026-05-15 13:08:57 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "src/server.ts" },
-    nitro: { preset: "vercel-edge" },
+    nitro: { 
+      preset: "vercel",
+      publicAssets: [
+        {
+          dir: "dist/client",
+          maxAge: 31536000
+        }
+      ]
+    },
   },
 });
